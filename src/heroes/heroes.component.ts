34b18e871a3668e6bb7bc9ec4ae8527d990bb501
DaniAkash <s.daniakash@gmail.com>
2017-02-01 12:32:50 +0530

    edit hero in seperate component

--- src/heroes/heroes.component.ts
+++ src/heroes/heroes.component.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from '../common/hero';
 import { HeroService } from '../services/hero.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-my-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  styleUrls: [ './heroes.component.css' ]
 })
 export class HeroesComponent implements OnInit {
   title = 'Tour of Heroes';
   selectedHero: Hero;
   heroes: Hero[];
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private router: Router,
+    private heroService: HeroService
+  ) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -27,5 +32,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
