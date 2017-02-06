420a09200a438ca01c460e8e295017090a4f9171
DaniAkash <s.daniakash@gmail.com>
2017-02-06 17:20:02 +0530

    remove unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { InMemoryDataService }  from '../data/in-memory-data.service';
